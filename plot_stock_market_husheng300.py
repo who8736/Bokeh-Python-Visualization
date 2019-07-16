6abd37a9e86392f2c7f403b3f939e572a7ee6f02
who8736 <who8736@hotmail.com>
2019-07-16 16:22:05 +0800

    上市指数成分分析

--- plot_stock_market_husheng300.py
+++ plot_stock_market_husheng300.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 =======================================
 Visualizing the stock market structure
@@ -76,6 +77,9 @@ from sklearn import cluster, covariance, manifold
 
 from sqlconn import engine
 
+from matplotlib.font_manager import FontProperties
+
+font_set = FontProperties(fname=r"c:\windows\fonts\simsun.ttc", size=12)
 
 # from sqlrw import readTTMPE
 
@@ -114,19 +118,130 @@ def readStockID():
 def myplot():
     df = pd.read_csv('index_chengfen.csv')
     # print(df)
-    symbols = df.stockid.to_list()
-    names = df.name.to_list()
+    symbols = df.stockid
+    names = df.name
+    # symbols = symbols[:10]
+    # names = names[:10]
     # print(symbols)
     # print(names)
+    variation = readPE(symbols)
+    print(variation)
+    print(variation.shape)
+
+    # #############################################################################
+    # Learn a graphical structure from the correlations
+    edge_model = covariance.GraphicalLassoCV(cv=5)
+
+    # standardize the time series: using correlations rather than covariance
+    # is more efficient for structure recovery
+    X = variation.copy().T
+    X /= X.std(axis=0)
+    edge_model.fit(X)
+
+    # #############################################################################
+    # Cluster using affinity propagation
+
+    _, labels = cluster.affinity_propagation(edge_model.covariance_)
+    n_labels = labels.max()
 
 
-def readPE():
+    for i in range(n_labels + 1):
+        print('Cluster %i: %s' % ((i + 1), ', '.join(names[labels == i])))
+
+    # #########################################################################
+    # Find a low-dimension embedding for visualization: find the best position
+    # of the nodes (the stocks) on a 2D plane
+
+    # We use a dense eigen_solver to achieve reproducibility (arpack is
+    # initiated with random vectors that we don't control). In addition, we
+    # use a large number of neighbors to capture the large-scale structure.
+    node_position_model = manifold.LocallyLinearEmbedding(
+        n_components=2, eigen_solver='dense', n_neighbors=6)
+
+    embedding = node_position_model.fit_transform(X.T).T
+    # embedding = node_position_model.fit_transform(X.T)
+
+    # #############################################################################
+    # Visualization
+    plt.figure(1, facecolor='w', figsize=(10, 8))
+    plt.clf()
+    plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
+    plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+    # noinspection PyTypeChecker
+    ax = plt.axes([0., 0., 1., 1.])
+    plt.axis('off')
+
+    # Display a graph of the partial correlations
+    partial_correlations = edge_model.precision_.copy()
+    d = 1 / np.sqrt(np.diag(partial_correlations))
+    partial_correlations *= d
+    partial_correlations *= d[:, np.newaxis]
+    non_zero = (np.abs(np.triu(partial_correlations, k=1)) > 0.02)
+
+    # Plot the nodes using the coordinates of our embedding
+    plt.scatter(embedding[0], embedding[1], s=100 * d ** 2, c=labels,
+                cmap=plt.cm.nipy_spectral)
+
+    # Plot the edges
+    start_idx, end_idx = np.where(non_zero)
+    # a sequence of (*line0*, *line1*, *line2*), where::
+    #            linen = (x0, y0), (x1, y1), ... (xm, ym)
+    segments = [[embedding[:, start], embedding[:, stop]]
+                for start, stop in zip(start_idx, end_idx)]
+    values = np.abs(partial_correlations[non_zero])
+    lc = LineCollection(segments,
+                        zorder=0, cmap=plt.cm.hot_r,
+                        norm=plt.Normalize(0, .7 * values.max()))
+    lc.set_array(values)
+    lc.set_linewidths(15 * values)
+    ax.add_collection(lc)
+
+    # Add a label to each node. The challenge here is that we want to
+    # position the labels to avoid overlap with other labels
+    for index, (name, label, (x, y)) in enumerate(
+            zip(names, labels, embedding.T)):
+
+        dx = x - embedding[0]
+        dx[index] = 1
+        dy = y - embedding[1]
+        dy[index] = 1
+        this_dx = dx[np.argmin(np.abs(dy))]
+        this_dy = dy[np.argmin(np.abs(dx))]
+        if this_dx > 0:
+            horizontalalignment = 'left'
+            x = x + .002
+        else:
+            horizontalalignment = 'right'
+            x = x - .002
+        if this_dy > 0:
+            verticalalignment = 'bottom'
+            y = y + .002
+        else:
+            verticalalignment = 'top'
+            y = y - .002
+        plt.text(x, y, name, size=10,
+                 horizontalalignment=horizontalalignment,
+                 verticalalignment=verticalalignment,
+                 bbox=dict(facecolor='w',
+                           edgecolor=plt.cm.nipy_spectral(
+                               label / float(n_labels)),
+                           alpha=.6))
+
+    plt.xlim(embedding[0].min() - .15 * embedding[0].ptp(),
+             embedding[0].max() + .10 * embedding[0].ptp(), )
+    plt.ylim(embedding[1].min() - .03 * embedding[1].ptp(),
+             embedding[1].max() + .03 * embedding[1].ptp())
+
+    plt.show()
+
+
+def readPE(ids):
     """
     返回每支股票每日pe涨跌，每股票占一行
     :return:
     """
     res = None
-    ids = ['600000', '600016', '600019', '600028']
+    # ids = ['600000', '600016', '600019', '600028']
     lens = len(ids)
     for i in range(lens):
         stockID = ids[i]
@@ -146,7 +261,7 @@ def readPE():
     cols = res.columns
     print(type(cols), cols)
     print(res.values.T.tolist())
-    return res.values.T.tolist()
+    return np.array(res.values.T.tolist())
 
 
 def _readPE(stockID='600000', startDate='20180101', endDate='20181231'):
@@ -272,6 +387,7 @@ def plotStockMarket():
     # The daily variations of the quotes are what carry most information
     variation = close_prices - open_prices
     print(variation)
+    print(variation.shape)
 
     # #############################################################################
     # Learn a graphical structure from the correlations
@@ -380,6 +496,8 @@ def plotStockMarket():
 if __name__ == '__main__':
     pass
     # downChengfen()
-    # myplot()
-    readPE()
+    myplot()
+    # arr = readPE()
+    # print(arr)
+    # print(arr.shape)
     # plotStockMarket()
